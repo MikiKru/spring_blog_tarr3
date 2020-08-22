7ae7cdae6cbc968d83982640d40a44994317acbb
MikiKru <michal_kruczkowski@o2.pl>
2020-08-22 11:09:12 +0200

    spring -v13

--- src/main/java/com/example/blog/controller/BlogController.java
+++ src/main/java/com/example/blog/controller/BlogController.java
@@ -1,13 +1,17 @@
 package com.example.blog.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller                  // klasa mapująca żądania protokołu http
 public class BlogController {
     @GetMapping("/")        // na adresie URL localhost:8080/
-    public String home(){   // wywoływana jest metoda zwracająca String
-        // implementacja ???
+    public String home(Model model){   // wywoływana jest metoda zwracająca String
+        // Model - klasa do przekazywania parametrów pomiędzy warstwą Front i Back -end
+        // model.addAttribute("nazwa obiektu w front-end", obiekt przekazywany z back_end);
+        model.addAttribute("header_title", "BLOG IT");
+        model.addAttribute("header_author", "Michal Kruczkowski");
         return "blog";      // wartością zwracaną jest nazwa szablony Thhymeleaf
                             // -> domyślna lokalizacja to resources/templates
                             // -> nie dopisujemy rozszerzenia .html
