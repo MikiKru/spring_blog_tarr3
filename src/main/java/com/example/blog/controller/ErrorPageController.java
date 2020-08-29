00372ac72d79cbdedd004c617b182f77e8ec21dc
MikiKru <michal_kruczkowski@o2.pl>
2020-08-29 11:23:29 +0200

    spring -v32

--- src/main/java/com/example/blog/controller/ErrorPageController.java
+++ src/main/java/com/example/blog/controller/ErrorPageController.java
@@ -1,17 +1,31 @@
 package com.example.blog.controller;
 
+import com.example.blog.service.BlogService;
+import com.example.blog.service.BlogServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class ErrorPageController implements ErrorController {
+    private BlogServiceImpl blogService;
+    @Autowired
+    public ErrorPageController(BlogServiceImpl blogService) {
+        this.blogService = blogService;
+    }
     @Override
     public String getErrorPath() {
-        return "redirect:/myErrorPage";
+        return "myErrorPage";
     }
     @GetMapping("/myErrorPage")
-    public String getErrorPage(){
+    public String getErrorPage(
+            Model model, Authentication auth
+    ){
+        model.addAttribute("auth", blogService.getLoginStatus(auth));
+        model.addAttribute("header_title", "ERROR 404");
         return "errorPage";
     }
 }
